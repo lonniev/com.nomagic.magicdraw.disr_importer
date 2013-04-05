1cb0f72480755c989af65f9cb480a5511dc87036
lonniev <lonniev@gmail.com>
2013-04-04 18:36:46 -0600

    Introduce to git the DISR Importer ruleset

---
+++ com.nomagic.magicdraw.disr_importer/mdw/com/sodius/mdw/metamodel/uml21/scripts/ClassScriptContainer_Standard.java
@@ -0,0 +1,4 @@
+package com.sodius.mdw.metamodel.uml21.scripts;
+
+public class ClassScriptContainer_Standard extends ClassScriptContainer {
+}
---
+++ com.nomagic.magicdraw.disr_importer/mdw/com/sodius/mdw/metamodel/uml21/scripts/ClassScriptContainer_UPDMElement.java
@@ -0,0 +1,4 @@
+package com.sodius.mdw.metamodel.uml21.scripts;
+
+public class ClassScriptContainer_UPDMElement extends ClassScriptContainer {
+}
